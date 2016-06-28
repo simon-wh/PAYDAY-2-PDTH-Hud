6f6e45442b31a15e361921aa3fda7a340fd1ddc8
GREAT BIG BUSHY BEARD <simon.whyte.lb@gmail.com>
2016-06-28 15:52:17 +0100

    Updated method of adding he challenges menu,

--- mods/PDTH Hud/Classes/Menu.lua
+++ mods/PDTH Hud/Classes/Menu.lua
@@ -1,61 +1,34 @@
-pdth_hud.challenges_menu = "pdth_hud_challenges"
-pdth_hud.active_challenges_menu = "pdth_hud_active_challenges"
-pdth_hud.completed_challenges_menu = "pdth_hud_completed_challenges"
-
-
-Hooks:Add("MenuManagerSetupCustomMenus", "Base_SetupPDTHHudMenu", function( menu_manager, nodes )
-    MenuHelper:NewMenu( pdth_hud.challenges_menu )
-    MenuHelper:NewMenu( pdth_hud.active_challenges_menu )
-    MenuHelper:NewMenu( pdth_hud.completed_challenges_menu )
-end)
-
-Hooks:Add("MenuManagerPopulateCustomMenus", "Base_PopulatePDTHHudMenu", function( menu_manager, nodes )
-    MenuCallbackHandler.active_Challenges_callback = function(this, item)
-        pdth_hud:refresh_active_challenges(pdth_hud.active_challenges_node)
-    end
-    MenuHelper:AddButton({
-        id = "active_challenges",
-        title = "pdth_hud_active_challenges",
-        desc = "pdth_hud_active_challenges_hint",
-        callback = "active_Challenges_callback",
-        next_node = pdth_hud.active_challenges_menu,
-        menu_id = pdth_hud.challenges_menu,
-        priority = 1
-    })
-
-    MenuCallbackHandler.completed_Challenges_callback = function(this, item)
-        pdth_hud:refresh_completed_challenges(pdth_hud.completed_challenges_node)
-    end
-    MenuHelper:AddButton({
-        id = "completed_challenges",
-        title = "pdth_hud_completed_challenges",
-        desc = "pdth_hud_completed_challenges_hint",
-        callback = "completed_Challenges_callback",
-        next_node = pdth_hud.completed_challenges_menu,
-        menu_id = pdth_hud.challenges_menu,
-        --priority = 2
-    })
-end)
-
-Hooks:Add("MenuManagerBuildCustomMenus", "Base_BuildPDTHHudMenu", function(menu_manager, nodes)
-    nodes[pdth_hud.challenges_menu] = MenuHelper:BuildMenu(pdth_hud.challenges_menu)
-    MenuHelper:AddMenuItem(nodes.main or nodes.pause, pdth_hud.challenges_menu, "pdth_hud_challenges", "pdth_hud_challenges_hint", "fbi_files", "after")
-
-    if nodes.lobby then
-        MenuHelper:AddMenuItem(nodes.lobby, pdth_hud.challenges_menu, "pdth_hud_challenges", "pdth_hud_challenges_hint", "fbi_files", "after")
-    end
-
-    nodes[pdth_hud.active_challenges_menu] = MenuHelper:BuildMenu(pdth_hud.active_challenges_menu)
-    pdth_hud.active_challenges_node = nodes[pdth_hud.active_challenges_menu]
-
-    nodes[pdth_hud.completed_challenges_menu] = MenuHelper:BuildMenu(pdth_hud.completed_challenges_menu)
-    pdth_hud.completed_challenges_node = nodes[pdth_hud.completed_challenges_menu]
-end)
-
-function pdth_hud:refresh_active_challenges(node)
-	if node == nil then
-		return
+PDTHHudChallenges = PDTHHudChallenges or class()
+function PDTHHudChallenges:refresh_node(node, data)
+	local selected_item = node:selected_item() and node:selected_item():name()
+	node = self:modify_node(node, data)
+	if selected_item then
+		node:select_item(selected_item)
 	end
+	return node
+end
+
+PDTHHudCompletedChallenges = PDTHHudCompletedChallenges or class(PDTHHudChallenges)
+function PDTHHudCompletedChallenges:modify_node(node, data)
+	node:clean_items()
+    for _,data in pairs(managers.challenges:get_completed()) do
+        local params = {
+                name = data.id,
+                text_id	= string.upper(data.name),
+                description_text = string.upper(data.description),
+                localize = "false",
+                challenge = data.id,
+                awarded	= true
+            }
+        local new_item = node:create_item({type = "MenuItemChallenge"}, params)
+        node:add_item(new_item)
+    end
+    managers.menu:add_back_button(node)
+	return node
+end
+
+PDTHHudActiveChallenges = PDTHHudActiveChallenges or class(PDTHHudChallenges)
+function PDTHHudActiveChallenges:modify_node(node, data)
 	node:clean_items()
 	for _,data in pairs(managers.challenges:get_near_completion()) do
 		local title_text = managers.challenges:get_title_text(data.id)
@@ -71,24 +44,5 @@ function pdth_hud:refresh_active_challenges(node)
 		node:add_item(new_item)
 	end
 	managers.menu:add_back_button(node)
-end
-
-function pdth_hud:refresh_completed_challenges(node)
-	if node == nil then
-		return
-	end
-	node:clean_items()
-	for _,data in pairs(managers.challenges:get_completed()) do
-		local params = {
-				name = data.id,
-				text_id	= string.upper(data.name),
-				description_text = string.upper(data.description),
-				localize = "false",
-				challenge = data.id,
-				awarded	= true
-			}
-		local new_item = node:create_item({type = "MenuItemChallenge"}, params)
-		node:add_item(new_item)
-	end
-	managers.menu:add_back_button(node)
+	return node
 end
