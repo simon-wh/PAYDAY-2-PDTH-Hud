c612cf7bfa2b659df2de72402034ddf7621d7774
GREAT BIG BUSHY BEARD <simon.whyte.lb@gmail.com>
2016-07-13 14:29:10 +0100

    Fixed a crash when the Main HUD is disabled.

--- mods/PDTH Hud/Hooks/NetworkAccountSteam.lua
+++ mods/PDTH Hud/Hooks/NetworkAccountSteam.lua
@@ -1,8 +1,10 @@
-local NetworkAccountSTEAMinventory_outfit_refresh = NetworkAccountSTEAM.inventory_outfit_refresh
+if pdth_hud.Options:GetValue("HUD/MainHud") then
+	local NetworkAccountSTEAMinventory_outfit_refresh = NetworkAccountSTEAM.inventory_outfit_refresh
 
-function NetworkAccountSTEAM:inventory_outfit_refresh()
-	NetworkAccountSTEAMinventory_outfit_refresh(self)
-    if managers.hud then
-        managers.hud._teammate_panels[HUDManager.PLAYER_PANEL]:get_weapon_info()
-    end
+	function NetworkAccountSTEAM:inventory_outfit_refresh()
+		NetworkAccountSTEAMinventory_outfit_refresh(self)
+	    if managers.hud and managers.hud._teammate_panels[HUDManager.PLAYER_PANEL] and managers.hud._teammate_panels[HUDManager.PLAYER_PANEL].get_weapon_info then
+	        managers.hud._teammate_panels[HUDManager.PLAYER_PANEL]:get_weapon_info()
+	    end
+	end
 end
